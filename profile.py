8b517abe83acd4c748b25d40aa719084136b80b6
jtmuir <43188969+jtmuir@users.noreply.github.com>
2018-09-17 17:53:26 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -18,7 +18,7 @@ pc = portal.Context()
 request = pc.makeRequestRSpec()
  
 # Add a raw PC to the request.
-node = request.XenVM("node")
+node = request.XenVM("node-1")
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
 node.routable_control_ip = "true"
 
