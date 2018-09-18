7c36d043726ca68b2f3fa0818c774902e46aace3
jtmuir <43188969+jtmuir@users.noreply.github.com>
2018-09-18 11:37:20 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -20,7 +20,6 @@ request = pc.makeRequestRSpec()
 # Add a raw PC to the request.
 
 node1 = request.XenVM("node-1")
-
 node1.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
 node1.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
 node1.routable_control_ip = "true"
@@ -30,7 +29,6 @@ iface1.component_id = "eth1"
 iface1.addAddress(pg.IPv4Address("192.168.1.1", "255.255.255.0"))
 
 node2 = request.XenVM("node-2")
-
 node2.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
 node2.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
 
@@ -38,10 +36,29 @@ iface2 = node2.addInterface("if2")
 iface2.component_id = "eth2"
 iface2.addAddress(pg.IPv4Address("192.168.1.2", "255.255.255.0"))
 
+node3 = request.XenVM("node-3")
+node3.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+node3.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+
+iface3 = node3.addInterface("if3")
+iface3.component_id = "eth3"
+iface3.addAddress(pg.IPv4Address("192.168.1.3", "255.255.255.0"))
+
+node4 = request.XenVM("node-4")
+
+node4.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+node4.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+
+iface4 = node4.addInterface("if4")
+iface4.component_id = "eth4"
+iface4.addAddress(pg.IPv4Address("192.168.1.4", "255.255.255.0"))
+
 link = request.LAN("lan")
 
 link.addInterface(iface1)
 link.addInterface(iface2)
+link.addInterface(iface3)
+link.addInterface(iface4)
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
